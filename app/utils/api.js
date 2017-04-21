7432cc496452bdf557be8e4bd381c24b2a4c5654
Matt Fehskens <matt.fehskens@gmail.com>
2017-04-21 17:28:00 -0400

    Added code to let us battle

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,40 @@
 const axios = require('axios');
 
+const id = 'YOUR_CLIENT_ID';
+const secret = 'YOUR_SECRET_ID';
+const params = `?client_id=${ id }&client_secret=${ secret }`;
+
+const battle = (players) =>
+    axios.all(players.map(getUserData))
+        .then(sortPlayers);
+
+const getUserData = (player) => axios.all([
+    getProfile(player),
+    getRepos(player)
+]).then((data) => ({
+    // profile is data[0], repos is data[1]
+    profile: data[0],
+    score: calculateScore(data[0], data[1])
+}));
+
+const getProfile = (username) => axios.get(`https://api.github.com/users/${ username }${ params }`)
+    .then((user) => user.data);
+const getRepos = (username) => axios.get(`https://api.github.com/users/${ username }/repos${ params }`);
+
+const calculateScore = (profile, repos) =>
+    (follower * 3) + getStarCount(repos);
+
+const getStarCount = (repos) => repos.reduce((count, repo) =>
+    count + repo.stargazers_count,
+    0
+);
+
+const handleError = (error) => {
+    console.warn(error);
+};
+
+const sortPlayers = (players) => players.sort((a, b) => b.score - a.score);
+
 const fetchPopularRepos = (language) => {
     const uri = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
 
@@ -8,5 +43,6 @@ const fetchPopularRepos = (language) => {
 };
 
 module.exports = {
+    battle,
     fetchPopularRepos
 }
